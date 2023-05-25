7bffbf7292121dd7820844b93eb0b38bb6c245bb
Paul Arntz <paul@paularntz.com>
2023-05-25 13:26:27 -0500

    more front page updates

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,7 @@
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return 
+    <div>
+      <img src="/ftld-logo-1.png" />
+      <h1 className="title">{title}</h1> 
+    </div>
 }
